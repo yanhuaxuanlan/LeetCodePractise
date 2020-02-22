f484edcb862b5589544c8bdf2fcedddcd131328a
wangfeng <wangfeng9036@adpanshi.com>
2020-02-22 17:42:14 +0800

    Initial Commit

---
+++ LeetCodePractise/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCodePractise
+//
+//  Created by simon on 2020/2/22.
+//  Copyright © 2020 simon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
